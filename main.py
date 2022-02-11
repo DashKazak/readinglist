ca709f24dc3ec2d2c9fe5b13243ca1142badd1fb
Dash <123qwe@gmail.com>
2022-02-11 15:49:21 -0600

    Removing a print statement

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from bookstore import Book, BookStore
 from menu import Menu
+from readinglist.bookstore import BookError
 
 import ui
 
@@ -34,14 +35,15 @@ def create_menu():
 
 
 def add_book():
-    new_book = ui.get_book_info()
     try: 
-        new_book.save() #the program will try to save the file
+        new_book = ui.get_book_info()
+        new_book.save()
+        
+         #the program will try to save the file
         # but, in bookstore.py line 107 we indicated that the book list can't have duplicates. If this error is encountered, the program will jump to the except statement below instead of printing long developer log error message. 
         
-    except: 
-        print(f'This book is already on your list - {new_book}')
-
+    except BookError as e: 
+        ui.message(e)
 def show_read_books():
     read_books = store.get_books_by_read_value(True)
     ui.show_books(read_books)
